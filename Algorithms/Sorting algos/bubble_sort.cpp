90a1062b0fea1f608d5531642401d0f4cb0eb6a7
Alok Kumar <56478877+Alok5102R@users.noreply.github.com>
2024-07-02 06:17:46 +0530

    Update bubble_sort.cpp

---
+++ Algorithms/Sorting algos/bubble_sort.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  vector<int> listnums = {23,578,4,7679,167,0,28};
+  int temp = 0, swap = 0;
+  // Time complexity = Best case:O(n), Worst case:O(n^2)
+  // Space complexity = O(1)
+
+  
+  // For Loop approach =====================
+  
+  for(int i=0;i<listnums.size();i++)
+  {
+   for(int j=0;j<listnums.size()-1;j++)
+   {
+   if(listnums[j]>listnums[j+1])
+   {
+   swap = 1;
+   temp = listnums[j];
+   listnums[j] = listnums[j+1];
+   listnums[j+1] = temp;
+   }
+   }
+   if(swap==0)
+   {
+   break;
+   }
+  }
+
+  
+   // While Loop approach =====================
+  
+   swap = 0;
+   while(swap==0)
+   {
+     for(int j=0;j<listnums.size()-1;j++)
+     {
+       if(listnums[j]>listnums[j+1])
+       {
+         swap = 1;
+         temp = listnums[j];
+         listnums[j] = listnums[j+1];
+         listnums[j+1] = temp;
+       }
+     }
+     
+     if(swap==0)
+     {
+       break;
+     }
+       else
+     {
+       swap=0;
+     }
+   }
+
+  
+  for(int i=0;i<listnums.size();i++)
+  {
+     cout<<listnums[i]<<"\n";
+  }
+  
+  return 0;
+}
